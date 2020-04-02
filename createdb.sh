5d3852386f69a44c29449b599500b00af6ada4dd
maruware <me@maruware.com>
2020-04-02 13:39:02 +0900

    Add charset

--- createdb.sh
+++ createdb.sh
@@ -3,7 +3,7 @@
 # databases=dev,test
 for db in ${MYSQL_DATABASES//,/ } ; do
     echo "Create Database: $db"  
-    echo "CREATE DATABASE IF NOT EXISTS \`$db\` ;" | "${mysql[@]}"
+    echo "CREATE DATABASE IF NOT EXISTS \`$db\` CHARACTER SET ${MYSQL_CHARSET:-utf8};" | "${mysql[@]}"
     echo "GRANT ALL ON \`$db\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
     echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
 done
